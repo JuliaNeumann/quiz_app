f6faa3a849c759664b8b31a57c459e233089482b
JuliaNeumann <jn.julianeumann@gmail.com>
2018-10-25 22:14:01 +0200

    add start for vue app

---
+++ quiz_app/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+        <header>
+            <h1>Welcome to my quiz app!</h1>
+            <p>The main purpose of this app is to try out different frontend technologies and thus help me learn them.</p>
+            <p>Secondly, this is also a quiz which can be played :-)</p>
+        </header>
+        <main>
+            <start-quiz />
+        </main>
+        <footer>
+            &copy; Julia Neumann, 2018
+        </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import StartQuiz from './components/StartQuiz.vue'
+
+export default {
+  name: 'app',
+  components: {
+    StartQuiz
+  }
+}
+</script>
+
+<style>
+  /*colors: http://paletton.com/#uid=13i0u0kbRt14+E48dwffUpTkImm*/
+  body {
+      font-family: 'Raleway', sans-serif;
+      color: white;
+      font-size: 18px;
+  }
+
+  .wrapper {
+      max-width: 1200px;
+      margin: 20px auto;
+      padding: 40px;
+      background-color: #669999;
+  }
+
+  h1, h2 {
+      font-family: 'Amatic SC', cursive;
+      color: #052929;
+  }
+
+  h1 {
+      margin-top: 0;
+  }
+
+  section {
+      margin-top: 40px;
+  }
+
+  footer {
+      text-align: right;
+      font-size: 16px;
+      color: #052929;
+  }
+</style>
---
+++ quiz_app/src/components/StartQuiz.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>
+        <h2>Let's get started ...</h2>
+        <form>
+            <p>Please select your quiz category and tell me how many questions you would like to be asked.</p>
+            <p class="inline_wrapper">
+                <label for="select_category">Category: </label>
+                <select id="select_category" name="select_category">
+                    <option value="0">Any</option>
+                </select>
+            </p>
+            <p class="inline_wrapper">
+                <label for="select_number_questions">Number of Questions: </label>
+                <select id="select_number_questions" name="select_number_questions">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
+            </p>
+            <p>
+                <input class="submit_prepare button" id="submit_prepare" name="submit_prepare" type="submit" value="Go!" />
+            </p>
+        </form>
+    </section>
+</template>
+
+<style>
+    .inline_wrapper {
+        display: inline-block;
+        margin-right: 20px;
+    }
+
+    .button {
+        font-family: 'Amatic SC', cursive;
+        font-size: 24px;
+        font-weight: bold;
+        background: none;
+        background-color: #052929;
+        border: 0;
+        color: inherit;
+        line-height: normal;
+        overflow: visible;
+        padding: 5px 10px;
+        -webkit-appearance: button; /* for input */
+        -webkit-user-select: none; /* for button */
+        -moz-user-select: none;
+        -ms-user-select: none;
+        cursor: pointer;
+    }
+
+    .button:hover,
+    .button:active,
+    .button:focus {
+        background-color: #0D4D4D;
+    }
+</style>
